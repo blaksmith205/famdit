0a58e93a4b51b6ad4992e50373d923a98ce48684
William Blanc <blaksmith57@gmail.com>
2024-04-13 18:35:58 -0400

    Updates tasks to show task info in dialog

--- src/app/components/TaskBlock.tsx
+++ src/app/components/TaskBlock.tsx
@@ -1,60 +1,101 @@
 "use client";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faInfo, faClockRotateLeft, faCheck} from '@fortawesome/free-solid-svg-icons'
+import { ClockIcon, CheckCircleIcon, InformationCircleIcon } from "@heroicons/react/24/outline";
+import { useState } from 'react';
+import InfoDialog from "./InfoDialog";
 
-export default function TaskBlock(prop : {taskName : string, amount : number, icons: Array<number>, date: Date}) {
-    function showInfo(event: MouseEvent) {
-        console.log("Show info...")
-        return <></>
+export enum TaskState {
+    Available, // Color with black text
+    Pending, // Color with yellow text
+    Credited, // Color with green text
+    Transfered, // Color with red text
+}
+
+export enum TaskAction {
+    Info, // Show extra information associated with the task
+    Approve, // Allow the task to be approved by the parent
+    Repeat, // Allow the task to be scheduled
+}
+
+export type Task = {
+    name: string;
+    amount: number;
+    parentView: boolean; // When true, the task is shown in the parent view
+    state: TaskState;
+    info: string;
+    actions?: TaskAction[];
+    completedOn?: Date;
+}
+
+function approve(task: Task) {
+    console.log("Approve task:", task);
+}
+
+function repeatTask(task: Task) {
+    console.log("Repeat task:", task);
+}
+
+function getStyle(task: Task) {
+    let style = "";
+    switch (task.state) {
+        case TaskState.Available:
+            style = "text-black-400";
+            break;
+        case TaskState.Pending:
+            style = "text-yellow-400";
+            break;
+        case TaskState.Credited:
+            style = "text-green-400";
+            break;
+        case TaskState.Transfered:
+            style = "text-red-400";
+            break;
     }
+    return style;
+}
+
+function buildButton(action: TaskAction, task: Task, stateChanger : (state: boolean) => void) {
+    let ref = null;
+
+    switch (action) {
+        case TaskAction.Info:
+            ref = {icon: InformationCircleIcon, handler: () => stateChanger(true)};
+            break;
+        case TaskAction.Approve:
+            ref = {icon: CheckCircleIcon, handler: () => approve(task)};
+            break;
+        case TaskAction.Repeat:
+            ref = {icon: ClockIcon, handler: () => repeatTask(task)};
+            break;
+    }
+    if (ref) {
+        return (
+            <button key={action} onClick={ref.handler}>
+                <ref.icon className="size-6 text-black-200 hover:text-black-100" aria-hidden="true"/>
+            </button>
+        )
+    } else {
+        return (<></>)
+    }
+}
+
+export default function TaskBlock({task}: {task: Task}) {
+    const [dialogOpen, setDialogOpen] = useState(false);
     
-    function approve(event: MouseEvent) {
-        console.log("Approve...")
-        return <></>        
-    }
-
-    function showDuration(event: MouseEvent) {
-        console.log("Show Duration...")
-        return <></>
-    }
-
-    function getIcon(val: number){
-        let icon = null;
-        let callback = null;
-        switch (val) {
-            case 0:
-                icon = faInfo;
-                callback = showInfo;
-                break;
-            case 1:
-                icon = faClockRotateLeft;
-                callback = showDuration;
-                break;
-            case 2:
-                icon = faCheck;
-                callback = approve;
-                break;
-        }
-        if (icon !== null) {
-            return <FontAwesomeIcon key={val} icon={icon} onClick={callback} border width="20px"/>
-        } else {
-            return <></>
-        }
-    }
     return (
-        <div className="gap-4 top-0 z-50 px-4 py-4 shadow-md bg-white">
-            <div className="flex gap-4 align-right">
-                <div>{prop.taskName}</div>
-                <div className='text-green'>${prop.amount}</div>
-                <div className='inline-flex flex-row'>
-                    {prop.icons?.map((icon) => 
-                        getIcon(icon)
-                    )}
-                </div>
+        <>
+        <div className="rounded-md p-2 shadow-md bg-white">
+            <div className='flex justify-end'>
+                {task.actions?.map((action) => buildButton(action, task, setDialogOpen))}
             </div>
-            <div>
-                Completed on: {prop.date.toLocaleDateString()}
+            <div className="border-b-2 p-1 flex">
+                <h1 className="flex-1">{task.name}</h1>
+                <div className={getStyle(task)}>${task.amount.toFixed(2)}</div>
+            </div>
+            <div className="text-xs">
+                Completed on: {task.completedOn ? task.completedOn.toLocaleDateString() : new Date().toLocaleDateString()}
             </div>
         </div>
+        <InfoDialog task={task} open={dialogOpen} setOpen={setDialogOpen}/>
+        </>
     )
 }
--- src/app/parent/page.tsx
+++ src/app/parent/page.tsx
@@ -1,16 +1,15 @@
 "use client";
-
-import TaskBlock from "../components/TaskBlock";
+import TaskBlock, {TaskState, createClickableInfo, apply, createClickableApprove, createClickableRepeat, TaskAction } from "../components/TaskBlock";
 
 export default function ParentPage() {
     return (
         <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
             <div className="flex-col ">
-            <div className="flex-row ">
-                <button className="">New Task</button>
-                <button className="">Edit Tasks</button>
-            </div>
-            <TaskBlock taskName={"Laundry"} amount={10} icons={[0,1,2]} date={new Date()}/>
+                <div className="flex-row ">
+                    <button className="">New Task</button>
+                    <button className="">Edit Tasks</button>
+                </div>
+                <TaskBlock task={{name:"Laundry", amount: 10, parentView: true, state: TaskState.Transfered, info: "Do your laundry every week.", actions: [TaskAction.Info, TaskAction.Approve, TaskAction.Repeat]}}/>
             </div>
       </div>
     )
