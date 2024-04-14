22cfb90c0f359ee7c64b4dcbe56f8c1ea9bcfc2d
William Blanc <blaksmith57@gmail.com>
2024-04-14 18:43:44 -0400

    Adds header to all pages

--- src/app/child/page.tsx
+++ src/app/child/page.tsx
@@ -4,6 +4,7 @@ import TaskBlock, {TaskState, TaskAction, Task, TaskConsumer } from "../componen
 import { User, UserContext } from "../components/accounts";
 import TransactionCard from "../components/transaction-card";
 import street_map from "./street_map_edited.png";
+import Header from "../components/header";
 
 export default function ChildPage() {
     let user = useContext(UserContext);
@@ -13,8 +14,11 @@ export default function ChildPage() {
 
     return (
         <div className="flex-col items-center justify-center overflow-y-auto">
+            <div className="m-auto w-1/2">
+                <Header user={user}/>
+            </div>
             <img className="w-1/2 m-auto pt-20 pb-5" src={street_map.src} alt="Street map view of local tasks" />
-            <div className="columns-2 w-full h-screen flex items-center justify-center gap-4">
+            <div className="columns-2 w-full flex items-center justify-center gap-4">
                 <div className="w-1/4 h-full">
                     <h1>Recent Transactions</h1>
                     {user.transactions && user.transactions.map((transaction) =>
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,43 +2,58 @@
 import { Menu } from '@headlessui/react'
 import { Fragment } from 'react'
 import { ChevronDownIcon, UserIcon } from '@heroicons/react/20/solid'
+import { User } from './accounts';
 
 export type UserRoute = {
     href: string;
     label: string;
-    isChild: boolean;
 }
 
-export default function Header({links}: {links: UserRoute[]}) {
+const routes = [
+    {href: "/", label: "Parent"}, 
+    {href: "/child", label: "John"},
+ ] as UserRoute[];
+
+export default function Header({user}: {user?: User}) {
+
     return (
-        <div className='fixed left-0 top-0 flex w-full justify-center pb-6 pt-8 gap-80'>
-            <a href='/'>
-                <UserIcon
-                    className="-mr-1 ml-2 h-20 w-20 text-gray-500 hover:text-black"
-                    aria-hidden="true"
-                />
+        <div className='grid grid-cols-3 mt-2 items-center justify-center'>
+            <a className="size-14 text-gray-500 hover:text-black" href='/'>
+                <UserIcon aria-hidden="true"/>
             </a>
-            <Menu as="div" className="relative inline-block text-left">
-                <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
-                    Sign In As
-                    <ChevronDownIcon
-                        className="-mr-1 ml-2 h-5 w-5 text-gray-500 hover:text-black"
-                        aria-hidden="true"
-                    />
-                </Menu.Button>
-                <Menu.Items
-                    className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
-                    {links.map((link) => (
-                        /* Use the `active` state to conditionally style the active item. */
-                        <Menu.Item key={link.href} as={Fragment}>
-                            {({ active }) => (
-                                <a href={link.href} className={`${active ? 'bg-blue-500 text-white' : 'bg-white text-black'}`}>{link.label}</a>
-                            )}
-                        </Menu.Item>
-                    ))}
-                </Menu.Items>
-            </Menu>
+            <div className='text-center'>
+                {user && <h1>Welcome {user.name}!</h1>}
+                {user && user.balance && 
+                    <div className='inline-flex flex-row gap-2'>
+                        <h3 className='text-sm text-black'>Available Balance</h3>
+                        <h3 className='text-sm text-green-400'>${user.balance.toFixed(2)}</h3>
+                    </div>
+                }
+            </div>
+            <div className="text-right">
+                <Menu as="div" className="relative inline-block text-left">
+                    <Menu.Button className="inline-flex justify-center rounded-md bg-blue-500 px-4 py-2 text-sm font-medium text-white hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
+                        Sign In As
+                        <ChevronDownIcon
+                            className="-mr-1 ml-2 h-5 w-5 text-gray-100 hover:text-black"
+                            aria-hidden="true"
+                        />
+                    </Menu.Button>
+                    <Menu.Items
+                        className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+                        <div className='px-1 py-1'>
+                            {routes.map((link) => (
+                                /* Use the `active` state to conditionally style the active item. */
+                                <Menu.Item key={link.href} as={Fragment}>
+                                    {({ active }) => (
+                                        <a href={link.href} className={`${active ? 'bg-blue-500 text-white' : 'bg-white text-black'} group flex w-full items-center rounded-md px-2 py-2 text-sm`}>{link.label}</a>
+                                    )}
+                                </Menu.Item>
+                            ))}
+                        </div>
+                    </Menu.Items>
+                </Menu>
+            </div>
         </div>
     )
-
 }
